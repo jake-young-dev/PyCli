389d34f39c8c2cddda3aa85b69267f05922f05b8
unknown <17075259+jake-young-dev@users.noreply.github.com>
2022-04-28 22:44:54 -0400

    Learning about packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='PyCli',
+   version='1.0',
+   description='A Python API Client',
+   author='jake-young-dev',
+   author_email='',
+   packages=['PyCli'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
